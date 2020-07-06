bcd1f5962690a406f9c84531aa12862ddf37e64d
Jonas Reichert <jonas.reichert@develappers.de>
2020-07-06 17:19:17 +0200

    #54 get list of taxes implemented

--- Develappers.BillomatNet/Helpers/TaxMappingExtensions.cs
+++ Develappers.BillomatNet/Helpers/TaxMappingExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 using Develappers.BillomatNet.Api;
 using Tax = Develappers.BillomatNet.Types.Tax;
 
@@ -13,6 +16,27 @@ namespace Develappers.BillomatNet.Helpers
             return value?.Tax.ToDomain();
         }
 
+        internal static Types.PagedList<Tax> ToDomain(this TaxList value)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            return new Types.PagedList<Tax>
+            {
+                Page = value.Page,
+                ItemsPerPage = value.PerPage,
+                TotalItems = value.Total,
+                List = value.List?.Select(x => x.ToDomain()).ToList()
+            };
+        }
+
+        internal static Types.PagedList<Tax> ToDomain(this TaxListWrapper value)
+        {
+            return value?.Item.ToDomain();
+        }
+
         private static Tax ToDomain(this Api.Tax value)
         {
             if (value == null)
@@ -23,8 +47,8 @@ namespace Develappers.BillomatNet.Helpers
             return new Tax
             {
                 Id = int.Parse(value.Id, CultureInfo.InvariantCulture),
-                Created = Convert.ToDateTime(value.Created),
-                Updated = Convert.ToDateTime(value.Updated),
+                Created = Convert.ToDateTime(value.Created, CultureInfo.InvariantCulture),
+                Updated = Convert.ToDateTime(value.Updated, CultureInfo.InvariantCulture),
                 Name = value.Name,
                 Rate = float.Parse(value.Rate),
                 IsDefault = value.IsDefault.ToBoolean()
