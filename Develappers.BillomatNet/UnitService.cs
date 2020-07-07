b726559df054c77a056c1c17585e6e2386ea6164
Martin Hey <martin.hey@develappers.de>
2020-07-07 12:09:29 +0200

    #51 added comments

--- Develappers.BillomatNet/UnitService.cs
+++ Develappers.BillomatNet/UnitService.cs
@@ -1,7 +1,6 @@
 ﻿using Develappers.BillomatNet.Api;
 using Develappers.BillomatNet.Helpers;
 using Develappers.BillomatNet.Queries;
-using Newtonsoft.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Unit = Develappers.BillomatNet.Types.Unit;
@@ -14,29 +13,65 @@ namespace Develappers.BillomatNet
         {
         }
 
+        /// <summary>
+        /// Retrieves a list of units.
+        /// </summary>
+        /// <param name="token">The cancellation token.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        /// The task result contains the list of units.
+        /// </returns>
         public Task<Types.PagedList<Unit>> GetListAsync(CancellationToken token = default(CancellationToken))
         {
             return GetListAsync(null, token);
         }
+
+        /// <summary>
+        /// Retrieves a list of units and applies the filter.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        /// <param name="token">The cancellation token.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        /// The task result contains the list of units.
+        /// </returns>
         public async Task<Types.PagedList<Unit>> GetListAsync(Query<Unit, UnitFilter> query, CancellationToken token = default(CancellationToken))
         {
             var jsonModel = await GetListAsync<UnitListWrapper>("/api/units", null, token).ConfigureAwait(false);
             return jsonModel.ToDomain();
         }
 
+        /// <summary>
+        /// Retrieves a unit item by it's id.
+        /// </summary>
+        /// <param name="id">The id.</param>
+        /// <param name="token">The cancellation token.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        /// The task result contains the unit.
+        /// </returns>
         public async Task<Unit> GetByIdAsync(int id, CancellationToken token = default(CancellationToken))
         {
             var jsonModel = await GetItemByIdAsync<UnitWrapper>($"/api/units/{id}", token).ConfigureAwait(false);
             return jsonModel.ToDomain();
         }
 
+
+        /// <summary>
+        /// Creates a unit.
+        /// </summary>
+        /// <param name="unit">The unit to create.</param>
+        /// <param name="token">The cancellation token.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        /// </returns>
         public async Task CreateAsync(Unit unit, CancellationToken token = default(CancellationToken))
         {
             var wrappedUnit = new UnitWrapper
             {
                 Unit = unit.ToApi()
             };
-            await PostAsync("/api/units", wrappedUnit, token);
+            await PostAsync("/api/units", wrappedUnit, token).ConfigureAwait(false);
         }
     }
 }
