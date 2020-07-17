8294db5046bd1a5d30a4f21a2f00b35699536d93
Jonas Reichert <jonas.reichert@develappers.de>
2020-07-17 16:20:37 +0200

    #5 edit article minor changes

--- Develappers.BillomatNet/Helpers/CommonMappingExtensions.cs
+++ Develappers.BillomatNet/Helpers/CommonMappingExtensions.cs
@@ -130,13 +130,13 @@ namespace Develappers.BillomatNet.Helpers
             };
         }
 
-        internal static string ToCultureString(this float? value)
+        internal static string ToInvariantString(this float? value)
         {
             if (value == null)
             {
                 return null;
             }
-            return float.Parse(value.ToString()).ToString(CultureInfo.InvariantCulture);
+            return value.Value.ToString(CultureInfo.InvariantCulture);
         }
     }
 }
