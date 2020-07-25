926ae1f52a5abf59b30ea175819d8b95bd9fbd20
Martin Hey <martin.hey@develappers.de>
2020-07-25 15:09:36 +0200

    changed base class for unit tests

--- Develappers.BillomatNet.Tests/UnitTests/ArticleServicePropertiesTest.cs
+++ Develappers.BillomatNet.Tests/UnitTests/ArticleServicePropertiesTest.cs
@@ -6,8 +6,7 @@ using Xunit;
 
 namespace Develappers.BillomatNet.Tests.UnitTests
 {
-    [Trait(Traits.Category, Traits.Categories.UnitTest)]
-    public class ArticleServicePropertiesTest
+    public class ArticleServicePropertiesTest : UnitTestBase<ArticleService>
     {
     }
 }
