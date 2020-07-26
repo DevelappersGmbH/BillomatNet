c7090fff0b04d12016343c2965dd06e54ba29aec
Martin Hey <martin.hey@develappers.de>
2020-07-26 13:09:36 +0200

    extracted query string generation to builders

---
+++ Develappers.BillomatNet/Queries/ArticleTagQueryStringBuilder.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using Develappers.BillomatNet.Types;
+
+namespace Develappers.BillomatNet.Queries
+{
+    internal class ArticleTagQueryStringBuilder : QueryStringBuilder<ArticleTag, Api.ArticleTag, ArticleTagFilter>
+    {
+        protected internal override string GetFilterStringFor(ArticleTagFilter filter)
+        {
+            if (filter == null)
+            {
+                return string.Empty;
+            }
+
+            var filters = new List<string>();
+            filters.Add($"article_id={filter.ArticleId}");
+
+            return string.Join("&", filters);
+        }
+    }
+}
---
+++ Develappers.BillomatNet/Queries/UnitQueryStringBuilder.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Web;
+using Develappers.BillomatNet.Types;
+
+namespace Develappers.BillomatNet.Queries
+{
+    internal class UnitQueryStringBuilder : QueryStringBuilder<Unit, Api.Unit, UnitFilter>
+    {
+        protected internal override string GetFilterStringFor(UnitFilter filter)
+        {
+            if (filter == null)
+            {
+                return string.Empty;
+            }
+
+            var filters = new List<string>();
+            if (!string.IsNullOrEmpty(filter.Name))
+            {
+                filters.Add($"?name={HttpUtility.UrlEncode(filter.Name)}");
+            }
+
+            return string.Join("&", filters);
+        }
+    }
+}
