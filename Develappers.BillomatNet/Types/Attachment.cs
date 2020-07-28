681be08e04d99146e659090ced5e16018f0594fd
Jonas Reichert <jonas.reichert@develappers.de>
2020-07-28 10:07:08 +0200

    #253 InvoiceAttachment als ByteArray minor changes

--- Develappers.BillomatNet/Types/Attachment.cs
+++ Develappers.BillomatNet/Types/Attachment.cs
@@ -4,6 +4,6 @@
     {
         public string Filename { get; set; }
         public string Mimetype { get; set; }
-        public byte[] Base64File { get; set; }
+        public byte[] BytesFile { get; set; }
     }
 }
