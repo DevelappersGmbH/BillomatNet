712d8ec567040562bc2af03bd7d742fb1e81ff24
Jonas Reichert <jonas.reichert@develappers.de>
2020-07-31 10:59:39 +0200

    #184 write operations on invoice comments fixed test

--- Develappers.BillomatNet.Tests/UnitTests/InvoiceServiceCommentsTests.cs
+++ Develappers.BillomatNet.Tests/UnitTests/InvoiceServiceCommentsTests.cs
@@ -234,11 +234,11 @@ namespace Develappers.BillomatNet.Tests.UnitTests
         public async Task Create_WithCorrectValues_ShouldCreateCommentAndReturnCorrectValues()
         {
             //arrange
-            var comment = new InvoiceComment { InvoiceId = 7506691, Created = DateTime.Parse("0001-01-01T00:00:00+02:00", CultureInfo.InvariantCulture),  Comment = "Test Comment", Public = true, ActionKey = CommentType.Comment, ByClient = true, UserId = 52821, ClientId = 3722360 };
+            var comment = new InvoiceComment { InvoiceId = 7506691, Created = DateTime.Parse("0001-01-01T00:00:00", CultureInfo.InvariantCulture),  Comment = "Test Comment", Public = true, ActionKey = CommentType.Comment, ByClient = true, UserId = 52821, ClientId = 3722360 };
             var expectedResult = new InvoiceComment { Id = 31327675, Created = DateTime.Parse("2020-07-30T10:42:51+02:00", CultureInfo.InvariantCulture), Comment = "Test Comment", ActionKey = CommentType.Comment, Public = true, ByClient = true, UserId = 52821, ClientId = 3722360, InvoiceId = 7506691 };
 
             var expectedRequestUri = new Uri("/api/invoice-comments", UriKind.Relative);
-            const string expectedRequestBody = "{\"invoice-comment\":{\"id\":\"0\",\"created\":\"0001-01-01T23:00:00.0000000+01:00\",\"comment\":\"Test Comment\",\"actionkey\":\"COMMENT\",\"public\":\"True\",\"by_client\":\"True\",\"user_id\":\"52821\",\"email_id\":\"\",\"client_id\":\"3722360\",\"invoice_id\":\"7506691\"}}";
+            const string expectedRequestBody = "{\"invoice-comment\":{\"id\":\"0\",\"created\":\"0001-01-01T00:00:00.0000000\",\"comment\":\"Test Comment\",\"actionkey\":\"COMMENT\",\"public\":\"True\",\"by_client\":\"True\",\"user_id\":\"52821\",\"email_id\":\"\",\"client_id\":\"3722360\",\"invoice_id\":\"7506691\"}}";
             const string responseBody = "{\"invoice-comment\":{\"id\":\"31327675\",\"created\":\"2020-07-30T10:42:51+02:00\",\"comment\":\"Test Comment\",\"actionkey\":\"COMMENT\",\"public\":\"1\",\"by_client\":\"1\",\"user_id\":\"52821\",\"email_id\":\"\",\"client_id\":\"3722360\",\"invoice_id\":\"7506691\",\"customfield\":\"\"}}";
 
             var http = A.Fake<IHttpClient>();
