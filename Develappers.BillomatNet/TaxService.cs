c1490720a213aff99ab2f5ce05256f5bb7335f3e
Martin Hey <martin.hey@develappers.de>
2020-07-31 17:54:25 +0200

    code cleanup

--- Develappers.BillomatNet/TaxService.cs
+++ Develappers.BillomatNet/TaxService.cs
@@ -17,7 +17,7 @@ namespace Develappers.BillomatNet
     public class TaxService : ServiceBase, IEntityService<Tax, TaxFilter>
     {
         /// <summary>
-        /// Creates a new instance of <see cref="TaxService"/>.
+        ///     Creates a new instance of <see cref="TaxService" />.
         /// </summary>
         /// <param name="configuration">The service configuration.</param>
         public TaxService(Configuration configuration) : base(configuration)
@@ -25,7 +25,7 @@ namespace Develappers.BillomatNet
         }
 
         /// <summary>
-        /// Creates a new instance of <see cref="TaxService"/> for unit tests.
+        ///     Creates a new instance of <see cref="TaxService" /> for unit tests.
         /// </summary>
         /// <param name="httpClientFactory">The function which creates a new <see cref="IHttpClient" /> implementation.</param>
         /// <exception cref="ArgumentNullException">Thrown when the parameter is null.</exception>
@@ -35,12 +35,12 @@ namespace Develappers.BillomatNet
         }
 
         /// <summary>
-        /// Retrieves a list of taxes.
+        ///     Retrieves a list of taxes.
         /// </summary>
         /// <param name="token">The cancellation token.</param>
         /// <returns>
-        /// A task that represents the asynchronous operation.
-        /// The task result contains the list of taxes.
+        ///     A task that represents the asynchronous operation.
+        ///     The task result contains the list of taxes.
         /// </returns>
         public async Task<Types.PagedList<Tax>> GetListAsync(CancellationToken token = default)
         {
@@ -48,20 +48,21 @@ namespace Develappers.BillomatNet
             return jsonModel.ToDomain();
         }
 
-        Task<Types.PagedList<Tax>> IEntityService<Tax, TaxFilter>.GetListAsync(Query<Tax, TaxFilter> query, CancellationToken token)
+        Task<Types.PagedList<Tax>> IEntityService<Tax, TaxFilter>.GetListAsync(Query<Tax, TaxFilter> query,
+            CancellationToken token)
         {
             // TODO: implement implicitly and make public
             throw new NotImplementedException();
         }
 
         /// <summary>
-        /// Retrieves a tax item by it's ID.
+        ///     Retrieves a tax item by it's ID.
         /// </summary>
         /// <param name="id">The ID.</param>
         /// <param name="token">The cancellation token.</param>
         /// <returns>
-        /// A task that represents the asynchronous operation.
-        /// The task result contains the tax.
+        ///     A task that represents the asynchronous operation.
+        ///     The task result contains the tax.
         /// </returns>
         public async Task<Tax> GetByIdAsync(int id, CancellationToken token = default)
         {
@@ -75,12 +76,12 @@ namespace Develappers.BillomatNet
         }
 
         /// <summary>
-        /// Deletes a tax.
+        ///     Deletes a tax.
         /// </summary>
         /// <param name="id">The ID.</param>
         /// <param name="token">The cancellation token.</param>
         /// <returns>
-        /// A task that represents the asynchronous operation.
+        ///     A task that represents the asynchronous operation.
         /// </returns>
         public async Task DeleteAsync(int id, CancellationToken token = default)
         {
@@ -93,13 +94,13 @@ namespace Develappers.BillomatNet
         }
 
         /// <summary>
-        /// Creates an tax.
+        ///     Creates an tax.
         /// </summary>
         /// <param name="value">The tax object.</param>
         /// <param name="token">The cancellation token.</param>
         /// <returns>
-        /// A task that represents the asynchronous operation.
-        /// The task result returns the newly created tax with the ID.
+        ///     A task that represents the asynchronous operation.
+        ///     The task result returns the newly created tax with the ID.
         /// </returns>
         public async Task<Tax> CreateAsync(Tax value, CancellationToken token = default)
         {
@@ -107,15 +108,13 @@ namespace Develappers.BillomatNet
             {
                 throw new ArgumentNullException(nameof(value));
             }
+
             if (string.IsNullOrEmpty(value.Name) || value.Id != 0)
             {
                 throw new ArgumentException("invalid property values for tax", nameof(value));
             }
 
-            var wrappedModel = new TaxWrapper
-            {
-                Tax = value.ToApi()
-            };
+            var wrappedModel = new TaxWrapper {Tax = value.ToApi()};
             var result = await PostAsync("/api/taxes", wrappedModel, token);
 
             return result.ToDomain();
