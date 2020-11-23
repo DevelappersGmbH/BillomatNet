6316831bbf42324749dad3f5d8983d304230a895
Jonas Reichert <jonas.reichert@develappers.de>
2020-11-23 17:14:45 +0100

    add test for creating InvoiceItem
    add to compare equality from InvoiceItem

---
+++ Develappers.BillomatNet.Tests/UnitTests/Comparer/InvoiceItemEqualityComparer.cs
@@ -0,0 +1,63 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using Develappers.BillomatNet.Types;
+
+namespace Develappers.BillomatNet.Tests.UnitTests.Comparer
+{
+    class InvoiceItemEqualityComparer : IEqualityComparer<InvoiceItem>
+    {
+        private const float FloatingPointTolerance = 0.0001f;
+
+        public bool Equals(InvoiceItem x, InvoiceItem y)
+        {
+            if (x == null && y == null)
+            {
+                return true;
+            }
+
+            if (x == null || y == null)
+            {
+                return false;
+            }
+
+            return x.Id == y.Id &&
+                x.ArticleId == y.ArticleId &&
+                x.InvoiceId == y.InvoiceId &&
+                x.Position == y.Position &&
+                x.Unit == y.Unit &&
+                x.Quantity == y.Quantity &&
+                Math.Abs(x.UnitPrice - y.UnitPrice) < FloatingPointTolerance &&
+                x.TaxName == y.TaxName &&
+                x.TaxRate == y.TaxRate &&
+                x.Description == y.Description &&
+                Math.Abs(x.TotalGross - y.TotalGross) < FloatingPointTolerance &&
+                Math.Abs(x.TotalNet - y.TotalNet) < FloatingPointTolerance &&
+                x.Reduction == y.Reduction &&
+                Math.Abs(x.TotalGrossUnreduced - y.TotalGrossUnreduced) < FloatingPointTolerance &&
+                Math.Abs(x.TotalNetUnreduced - y.TotalNetUnreduced) < FloatingPointTolerance;
+        }
+
+        public int GetHashCode(InvoiceItem obj)
+        {
+            return obj.Id.GetHashCode() ^
+                obj.ArticleId.GetHashCode() ^
+                obj.InvoiceId.GetHashCode() ^
+                obj.Position.GetHashCode() ^
+                obj.Unit.GetHashCode() ^
+                obj.Quantity.GetHashCode() ^
+                obj.UnitPrice.GetHashCode() ^
+                obj.TaxName.GetHashCode() ^
+                obj.TaxRate.GetHashCode() ^
+                obj.Description.GetHashCode() ^
+                obj.TotalGross.GetHashCode() ^
+                obj.TotalNet.GetHashCode() ^
+                obj.Reduction.GetHashCode() ^
+                obj.TotalGrossUnreduced.GetHashCode() ^
+                obj.TotalNetUnreduced.GetHashCode();
+        }
+    }
+}
--- Develappers.BillomatNet.Tests/UnitTests/InvoiceServiceItemsTest.cs
+++ Develappers.BillomatNet.Tests/UnitTests/InvoiceServiceItemsTest.cs
@@ -2,9 +2,73 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Develappers.BillomatNet.Api.Net;
+using Develappers.BillomatNet.Tests.UnitTests.Comparer;
+using Develappers.BillomatNet.Types;
+using FakeItEasy;
+using FluentAssertions;
+using Xunit;
+
 namespace Develappers.BillomatNet.Tests.UnitTests
 {
     public class InvoiceServiceItemsTest : UnitTestBase<InvoiceService>
     {
+        [Fact]
+        public async Task Create_WithCorrectValues_ShouldCreateItemAndReturnCorrectValues()
+        {
+            //arrange
+            var expectedRequestUri = new Uri("/api/invoice-items", UriKind.Relative);
+            const string expectedRequestBody = "{\"invoice-item\":{\"id\":\"0\",\"article_id\":\"154123\",\"invoice_id\":\"8332909\",\"position\":\"1\",\"quantity\":\"3.54\",\"title\":\"Test-Item\",\"description\":\"Ganz viel toller Text\",\"reduction\":\"0\"}}";
+            const string responseBody = "{\"invoice-item\":{\"id\":\"20643891\",\"article_id\":\"154123\",\"invoice_id\":\"8332909\",\"position\":\"1\",\"unit\":\"Stück\",\"quantity\":\"3.54\",\"type\":\"\",\"unit_price\":\"2\",\"tax_name\":\"MwSt\",\"tax_rate\":\"19\",\"title\":\"Test-Item\",\"description\":\"Ganz viel toller Text\",\"total_net\":\"7.08\",\"total_gross\":\"8.4252\",\"reduction\":\"\",\"total_net_unreduced\":\"7.0778\",\"total_gross_unreduced\":\"8.4226\",\"customfield\":\"\"}}";
+
+            var model = new InvoiceItem
+            {
+                ArticleId = 154123,
+                InvoiceId = 8332909,
+                Position = 1,
+                Quantity = 3.5389F,
+                Title = "Test-Item",
+                Description = "Ganz viel toller Text"
+            };
+            var expectedResult = new InvoiceItem
+            {
+                Id = 20643891,
+                ArticleId = 154123,
+                InvoiceId = 8332909,
+                Position = 1,
+                Unit = "Stück",
+                Quantity = 3.54F,
+                UnitPrice = 2,
+                TaxName = "MwSt",
+                TaxRate = 19,
+                Title = "Test-Item",
+                Description = "Ganz viel toller Text",
+                TotalNet = 7.08F,
+                TotalGross = 8.4252F,
+                Reduction = null,
+                TotalNetUnreduced = 7.0778F,
+                TotalGrossUnreduced = 8.4226F
+            };
+
+            var http = A.Fake<IHttpClient>();
+            A.CallTo(() => http.PostAsync(expectedRequestUri, expectedRequestBody, A<CancellationToken>.Ignored))
+                .Returns(Task.FromResult(responseBody));
+
+            var sut = GetSystemUnderTest(http);
+
+            Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            //act
+            var result = await sut.CreateItemAsync(model);
+
+            // assert
+            A.CallTo(() => http.PostAsync(expectedRequestUri, expectedRequestBody, A<CancellationToken>.Ignored))
+                .MustHaveHappenedOnceExactly();
+
+            result.Should().BeEquivalentUsingComparerTo(expectedResult, new InvoiceItemEqualityComparer());
+        }
     }
 }
