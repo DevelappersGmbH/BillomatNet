e6438ccd8346b522f25d4bf71ebc43a1cd158b20
Martin Hey <martin.hey@develappers.de>
2024-01-09 17:52:16 +0100

    fixed build error in letterservice

--- Develappers.BillomatNet/LetterService.cs
+++ Develappers.BillomatNet/LetterService.cs
@@ -32,7 +32,6 @@ namespace Develappers.BillomatNet
                 throw new ArgumentException("invalid letter id", nameof(id));
             }
 
-            return $"{HttpClient.BaseUrl}app/{EntityUrlFragment}/show/entityId/{id}";
             return $"{HttpClient.BaseUrl}app/beta/{EntityUrlFragment}/{id}";
         }
     }
